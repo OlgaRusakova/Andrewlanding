720d192ec8f35dce44140e092af7f6c00aa8ddd4
unknown <a.v.rusakou@gmail.com>
2021-04-18 16:45:59 +0300

    burger

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
+const burgerEl = document.querySelector('.header-top__burger');
+const menuWrapEl = document.querySelector('.header-top__nav');
+
+burgerEl.addEventListener('click', function(event) {
+  menuWrapEl.classList.toggle('header-top__nav_visible');
+});
+
 var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    },
     navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
     },
 });
